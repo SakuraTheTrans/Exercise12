4bcfad63b0750e11edd936ccc77550a53a72fe51
Sakura <sakurathetrans@gmail.com>
2025-04-08 13:28:57 -0400

    Code!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Kinda hated this ;w;
+
+        Queue<String> strings = new Queue<>(3);
+        try{
+            strings.enqueue("Goodbye");
+            strings.enqueue("World");
+            strings.enqueue("!!!");
+            System.out.println(strings.dequeue());
+            System.out.println(strings.dequeue());
+            System.out.println(strings.dequeue());
+        } catch (StackFullException | StackEmptyException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+class StackFullException extends Exception {}
 
-public class Queue {
+class StackEmptyException extends Exception {}
+
+class Queue<E> {
+    private E[] elements;
+    private int index = 0;
+    private int size;
+
+    Queue(int size){
+        elements = (E[]) new Object[size];
+        this.size = size;
+    }
+
+    public void enqueue(E element) throws StackFullException {
+        if (index >= size) {
+            throw new StackFullException();
+        }
+
+        elements[index] = element;
+        index++;
+    }
+
+    E dequeue() throws StackEmptyException {
+        if (index == 0) {
+            throw new StackEmptyException();
+        }
+        
+        E returnElement = elements[0];
+
+        for (int i = 1; i < index; i++) {
+            elements[i - 1] = elements[i];
+        }
+        
+        index--;
+        return returnElement;
+    }
+    
+    public void test(){
+        System.out.println("Hello World");
+    }
 }
